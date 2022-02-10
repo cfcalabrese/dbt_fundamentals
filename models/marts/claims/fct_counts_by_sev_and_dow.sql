c3732788edac7d1421fc96c04acc0c1c5dce5f0f
chris.calabrese@esure.com <chris.calabrese@esure.com>
2022-02-10 21:52:13 +0000

    Create fct_counts_by_sev_and_dow table

---
+++ models/marts/claims/fct_counts_by_sev_and_dow.sql
@@ -0,0 +1,51 @@
+with int_table as (
+    select * from {{ ref('int_severity_time_road') }}
+),
+
+rt as (
+    select 
+        cast(val as int) as road_type,
+        label as road_type_name
+        
+    from {{ ref('stg_lookup_accident') }}
+    where column_name = 'road_type'
+),
+
+acc_sev as (
+    select 
+        cast(val as int) as accident_severity,
+        label as accident_severity_name
+        
+    from {{ ref('stg_lookup_accident') }}
+    where column_name = 'accident_severity'    
+),
+
+dow as (
+    select 
+        cast(val as int) as day_of_week,
+        label as day_of_week_name
+        
+    from {{ ref('stg_lookup_accident') }}
+    where column_name = 'day_of_week'
+)
+
+select 
+    count(*),
+    int_table.casualty_severity as casualty_severity,
+    acc_sev.accident_severity_name as accident_severity,
+    dow.day_of_week_name as day_of_week
+    #rt.road_type_name as road_type
+from int_table
+left join acc_sev using (accident_severity)
+left join rt using (road_type)
+left join dow using (day_of_week)
+
+group by 
+    casualty_severity,
+    accident_severity,
+    day_of_week
+    #road_type
+
+order by 
+    day_of_week asc,
+    casualty_severity asc
